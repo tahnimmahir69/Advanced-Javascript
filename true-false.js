6765b53dae82045faa1a250c3900c72943355c91
Tahnim-Mahir <tahnimmahir@gmail.com>
2020-04-27 20:50:02 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,31 @@
+//const name = "mahir";  //true
+//const name = ""; //false
+//const name = " ";  //true
+//const name = 0;   //false
+//const name = "0";   //true
+
+// let name;
+// console.log(name);  //Undifined =. flase
+
+// let name = null;
+// console.log(name);    //condition == false
+
+//Nan means Not a Number
+
+// let name =NaN;
+// console.log(name)    //false
+
+// if(name){
+//     console.log("condition true");
+// }
+// else{
+//     console.log("condition false");
+// }
+
+let name = 0;
+if( name || name == 0){   // consition true
+    console.log("condition true");
+}
+else{
+    console.log("condition false");
+}
