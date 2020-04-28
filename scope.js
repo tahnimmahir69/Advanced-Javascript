b0d7cfb90109517bdb622762c7e07eb28165ba11
Tahnim-Mahir <tahnimmahir@gmail.com>
2020-04-28 21:31:38 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,65 @@
+// let bonus = 20;
+
+// function sum(first, second){
+//     let result = first + second + bonus;
+//     console.log(bonus);
+//     return result;
+// }
+
+// const  output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+
+//OR
+
+// let bonus = 20;
+
+// function sum(first, second){
+//     let result = first + second + bonus;
+//     if(result >9){
+//         const mood = "happy";
+//         console.log(mood);
+//     }
+    
+//     return result;
+// }
+
+// const  output = sum(3, 7);
+
+//OR
+// function sum(first, second){
+//     let result = first + second + bonus;
+//     if(result >9){
+//         const mood = "happy";
+//         mood = "firske";
+//         mood = "enjoy";
+//         mood = "entertainment"
+        
+    
+//     return result;
+//     console.log(mood);   // this time error when we console log in the other space
+//     }
+// }
+
+// const  output = sum(3, 7);
+
+//OR
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    if(result >9){
+        var mood = "happy";
+        mood = "firske";
+        mood = "enjoy";
+        mood = "entertainment";
+        
+        }
+
+    console.log(day); 
+    var day = "friday";
+    return result;
+}
+
+
+
+
